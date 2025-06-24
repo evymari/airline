7d6bb175533059b347123bae62fe8a99b07abda0
evymari <144184061+evymari@users.noreply.github.com>
2025-06-24 07:03:31 +0200

    Add login system with authentication logic and validations

--- src/main/java/com/f5/Airline/users/UserRepository.java
+++ src/main/java/com/f5/Airline/users/UserRepository.java
@@ -14,16 +14,3 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 }
 
-
-/*
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
-
-
-}*/
